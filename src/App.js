ffa5f26e1be9218083b70a85dc1ad0a1a0ff29e2
Stefano Casasola <stepanello@gmail.com>
2020-02-05 10:16:58 +0100

    Adds manifest, assets, head tags

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./strive-logo.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>STRIVE PWA YEEEEEE!!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
